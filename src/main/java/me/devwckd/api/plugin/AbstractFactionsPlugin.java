17145b8d53f1acfcc05f1941c21dd28c1c59bc77
devwckd <dev.wckd@gmail.com>
2020-09-09 11:50:45 -0300

    First Commit 🍕

---
+++ src/main/java/me/devwckd/api/plugin/AbstractFactionsPlugin.java
@@ -0,0 +1,9 @@
+package me.devwckd.api.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @copyright github.com/devwckd
+ */
+public class AbstractFactionsPlugin extends JavaPlugin {
+}
---
+++ src/main/java/me/devwckd/factions/FactionsPlugin.java
@@ -0,0 +1,9 @@
+package me.devwckd.factions;
+
+import me.devwckd.api.plugin.AbstractFactionsPlugin;
+
+/**
+ * @copyright github.com/devwckd
+ */
+public class FactionsPlugin extends AbstractFactionsPlugin {
+}
