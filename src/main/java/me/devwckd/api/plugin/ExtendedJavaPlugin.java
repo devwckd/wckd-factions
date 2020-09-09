27fbc833d30071349e6db6628973c9e06a04d568
devwckd <dev.wckd@gmail.com>
2020-09-09 11:56:53 -0300

    Made ExtendedJavaPlugin's base.

--- src/main/java/me/devwckd/api/plugin/ExtendedJavaPlugin.java
+++ src/main/java/me/devwckd/api/plugin/ExtendedJavaPlugin.java
@@ -7,4 +7,22 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class ExtendedJavaPlugin extends JavaPlugin {
 
+    @Override
+    public final void onLoad() {
+        load();
+    }
+
+    @Override
+    public final void onDisable() {
+        enable();
+    }
+
+    @Override
+    public final void onEnable() {
+        disable();
+    }
+
+    public void load() { }
+    public void enable() { }
+    public void disable() { }
 }
